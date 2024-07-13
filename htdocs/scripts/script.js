071fd030ae4695631249d3c0b0c0096dd9353456
VesperG92 <gloriafrancocu@gmail.com>
2024-07-13 12:52:54 +0200

    Adding cards instead of a carousel

--- htdocs/scripts/script.js
+++ htdocs/scripts/script.js
@@ -1,3 +1,9 @@
-document.getElementById('toggleTheme').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
+document.getElementById("toggleTheme").addEventListener("click", function() {
+    // Cambiar a modo oscuro
+    document.body.classList.toggle("dark-mode");
+    
+    // Oculta la sección de introducción
+    document.querySelector(".intro-text").style.display = "none";
+    document.querySelector(".intro-subtext").style.display = "none";
+    document.querySelector(".projects-container").style.display = "flex";
 });
